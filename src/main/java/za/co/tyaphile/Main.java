289c5ac8a656deecf975c2db146ccc1831621026
Thulani Tyaphile <tj.tyaphile@gmail.com>
2024-08-18 18:57:39 +0200

    Update user-agent to Linux

--- src/main/java/za/co/tyaphile/Main.java
+++ src/main/java/za/co/tyaphile/Main.java
@@ -62,7 +62,7 @@ public class Main {
                 List.of("disable-popup-blocking"));
         options.addArguments("--headless");
         options.addArguments(
-                "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+                "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36");
 
 //        options.addArguments("--window-size=1400,1200");
 //        options.addArguments("--disable-gpu");
